0241d612f14f0d659bd15a742f8e03f6e79832d0
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2025-11-09 18:20:29 -0600

    Fix input names

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const nameInput = document.getElementById("name"); //name input
 const dateInput = document.getElementById("date"); //date input
 const emailInput = document.getElementById("email"); //email input
 const websiteInput = document.getElementById("website"); //website input
-const phoneInput = document.getElementById("phone"); //phone input
+const phoneInput = document.getElementById("phone"); //phone number input
 const form = document.getElementById("form"); //form element
 submitButton.addEventListener("click", (e) => {
     //when submit button is clicked
